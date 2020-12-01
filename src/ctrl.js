574a885093c3c9e33f134488868ea9f04a9f5446
Jamie Schouten <jamie@qlic.nl>
2020-12-01 17:13:57 +0100

    sdfjasld

--- src/ctrl.js
+++ src/ctrl.js
@@ -153,7 +153,7 @@ export class GroupedBarChartCtrl extends MetricsPanelCtrl {
                 // this.element.clientHeigh;
 
                 this.width = this.element.parentNode.clientWidth
-                this.height = this.element.parentNode.clientHeight * 0.7
+                this.height = this.element.parentNode.clientHeight
 
                 this.svg.attr('width', this.width)
                     .attr('height', this.height)
